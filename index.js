7d8ef31d21b6e3cb3800600ab127dbc7b5b6fce9
Juho Hänninen <juho.hanninen@cgi.com>
2022-04-08 14:13:04 +0300

    Use dummy data if Liipi is down

--- index.js
+++ index.js
@@ -4,20 +4,29 @@ const vtPbf = require('vt-pbf');
 const request = require('requestretry');
 const zlib = require('zlib');
 
+const dummyData = { "type": "Feature", "properties": {}, "geometry": null }; // Empty geometry to be used if Liipi is down.
+
 const getTileIndex = (url, callback) => {
   request({
     url: url,
     maxAttempts: 20,
     retryDelay: 30000
   }, function (err, res, body){
-    if (err){
-      callback(err);
+    if (err || !body) {
+      console.log("Error fetching data from LIIPI. Using dummy data...", err)
+      callback(null, geojsonVt(dummyData));
       return;
+    } 
+
+    try {
+      callback(null, geojsonVt(JSON.parse(body), {
+        maxZoom: 20,
+        buffer: 1024,
+      })); //TODO: this should be configurable)
+    } catch(e) {
+      console.log("Error in data request body. Using dummy data...", e)
+      callback(null, geojsonVt(dummyData));
     }
-    callback(null, geojsonVt(JSON.parse(body), {
-      maxZoom: 20,
-      buffer: 1024,
-    })); //TODO: this should be configurable)
   })
 }
 
@@ -51,7 +60,7 @@ class GeoJSONSource {
     if (hubTile === null){
       hubTile = {features: []}
     }
-    
+
     const data = Buffer.from(vtPbf.fromGeojsonVt({facilities: facilityTile, hubs: hubTile}));
 
     zlib.gzip(data, function (err, buffer) {
